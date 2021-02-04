790bd4d1835122367d2edb592ddab6cb4197e02b
Levishery <cqh@mail.ustc.edu.cn>
2021-02-04 12:41:13 +0800

    begin implementing Ranking Constrain Loss

--- 5_data/MP_train.py
+++ 5_data/MP_train.py
@@ -1,15 +1,11 @@
-import torch
-from torch_geometric.data import Data
-
+from utils import *
 import torch
 import torch.nn.functional as F
 from torch.nn import Sequential, Linear, ReLU, GRU
-import matplotlib.pyplot as plt
-import pandas as pd
 from torch_geometric.nn import NNConv, Set2Set
 from torch_geometric.data import Data, DataLoader
 import numpy as np
-import os
+
 import argparse
 
 N = 5
@@ -130,63 +126,6 @@ def loadData(test_only, data_path, pretrain):
 
     return train_loader, test_loader, [std, mean]
 
-
-def plotCurves(train_MSE, test_MSE, EPOCH):
-    exp_MSE = 0.000015794911
-    print(f"best test MSE: {np.min(test_MSE):.12f};   experiment MSE error: 0.000015794911;   data variance: "
-          f"0.045936428010")
-    exp_MSE = np.full(len(train_MSE), exp_MSE)
-    plt.plot(train_MSE[1:], 'r', label="train")
-    plt.plot(test_MSE[1:], 'b', label="test")
-    plt.plot(exp_MSE[1:], '-', label="experiment")
-    plt.xlabel('epoch')
-    plt.ylabel('Mean Square Error')
-    plt.legend()
-    plt.savefig('train_MP.png')
-
-    plt.clf()
-
-    plt.plot(train_MSE[EPOCH // 2:], 'r', label="train")
-    plt.plot(test_MSE[EPOCH // 2:], 'b', label="test")
-    plt.plot(exp_MSE[EPOCH // 2:], '-', label="experiment")
-    plt.xlabel('epoch')
-    plt.ylabel('Mean Square Error')
-    plt.legend()
-    plt.savefig('train_MP2.png')
-
-
-def savePrediction(data, prediction, std, mean):
-    result = np.zeros([len(data.y), 2])
-    prediction = prediction * std + mean
-    prediction = prediction.cpu().detach().numpy()
-    result[:, 0] = prediction[0:len(data.y), 0]  # pre
-    result[:, 1] = np.asarray([d * std + mean for d in data.y.cpu()])
-
-    data = pd.DataFrame(result)
-
-    writer = pd.ExcelWriter('MP_result.xlsx')  # 写入Excel文件
-    data.to_excel(writer, 'page_1', float_format='%.9f')  # ‘page_1’是写入excel的sheet名
-    writer.save()
-
-    writer.close()
-
-
-def printInstance(data, prediction, std, mean):
-    result = np.zeros([len(data.y), 2])
-    prediction = prediction * std + mean
-    prediction = prediction.cpu().detach().numpy()
-    result[:, 0] = prediction[0:len(data.y), 0]  # pre
-    result[:, 1] = np.asarray([d * std + mean for d in data.y.cpu()])
-    print('w:')
-    print(data.x[0])
-    print('a_upper:')
-    print(data.edge_attr[0][:, 0])
-    print('a_lower:')
-    print(data.edge_attr[0][:, 1])
-    print('gt:')
-    print(result[0, 1])
-    print('prediction')
-    print(prediction[0])
 
 def main():
 
---
+++ 5_data/utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plotCurves(train_MSE, test_MSE, EPOCH):
+    exp_MSE = 0.000015794911
+    print(f"best test MSE: {np.min(test_MSE):.12f};   experiment MSE error: 0.000015794911;   data variance: "
+          f"0.045936428010")
+    exp_MSE = np.full(len(train_MSE), exp_MSE)
+    plt.plot(train_MSE[1:], 'r', label="train")
+    plt.plot(test_MSE[1:], 'b', label="test")
+    plt.plot(exp_MSE[1:], '-', label="experiment")
+    plt.xlabel('epoch')
+    plt.ylabel('Mean Square Error')
+    plt.legend()
+    plt.savefig('train_MP.png')
+
+    plt.clf()
+
+    plt.plot(train_MSE[EPOCH // 2:], 'r', label="train")
+    plt.plot(test_MSE[EPOCH // 2:], 'b', label="test")
+    plt.plot(exp_MSE[EPOCH // 2:], '-', label="experiment")
+    plt.xlabel('epoch')
+    plt.ylabel('Mean Square Error')
+    plt.legend()
+    plt.savefig('train_MP2.png')
+
+
+def savePrediction(data, prediction, std, mean):
+    result = np.zeros([len(data.y), 2])
+    prediction = prediction * std + mean
+    prediction = prediction.cpu().detach().numpy()
+    result[:, 0] = prediction[0:len(data.y), 0]  # pre
+    result[:, 1] = np.asarray([d * std + mean for d in data.y.cpu()])
+
+    data = pd.DataFrame(result)
+
+    writer = pd.ExcelWriter('MP_result.xlsx')  # 写入Excel文件
+    data.to_excel(writer, 'page_1', float_format='%.9f')  # ‘page_1’是写入excel的sheet名
+    writer.save()
+
+    writer.close()
+
+
+def printInstance(data, prediction, std, mean):
+    result = np.zeros([len(data.y), 2])
+    prediction = prediction * std + mean
+    prediction = prediction.cpu().detach().numpy()
+    result[:, 0] = prediction[0:len(data.y), 0]  # pre
+    result[:, 1] = np.asarray([d * std + mean for d in data.y.cpu()])
+    print('w:')
+    print(data.x[0])
+    print('a_upper:')
+    print(data.edge_attr[0][:, 0])
+    print('a_lower:')
+    print(data.edge_attr[0][:, 1])
+    print('gt:')
+    print(result[0, 1])
+    print('prediction')
+    print(prediction[0])
